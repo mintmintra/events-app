9d55e4acdc1ee6c09c24b08f05fd07ef9bb2a5d5
mintmintra <mintracss019@gmail.com>
2023-01-09 01:26:53 +0700

    first commit

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,6 +1,14 @@
 const Page = () => {
     return (
-        <h1>About us Page</h1>
+        <div>
+            <h1>About us Page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, dignissimos ducimus corporis possimus similique tenetur. Magnam fugiat ex quas. Veniam ut dolore maxime officia ipsa cupiditate adipisci aut voluptatum minus?
+                Nisi omnis harum qui repudiandae obcaecati accusantium! Similique nisi unde doloremque velit repellendus nobis minima maiores qui dignissimos, obcaecati eaque porro corrupti assumenda soluta quas. Dolorum ullam reiciendis odio. Suscipit.
+                Quis ratione a quo vitae! Inventore illo, a ad culpa quidem ut esse asperiores assumenda, hic sit deserunt deleniti ullam similique voluptatem ab vel minus veritatis accusantium atque. Ipsa, deserunt!
+                Vitae voluptatibus ipsa iusto quia nulla aperiam eligendi! Quaerat ab praesentium reiciendis aperiam assumenda rerum, iste quia repellat at error non! Exercitationem, nihil accusamus. Odit molestias neque illum harum hic.
+            </p>
+        </div>
     )
 }
 export default Page;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,22 @@
 const Page = () => {
-    return (
+    return <div>
         <h1>Event Page</h1>
-    )
+        <div>
+        <a href="">
+          <img src="" alt="" />
+          <h2>Event in London</h2>
+          
+        </a>
+        <a href="">
+          <img src="" alt="" />
+          <h2>Event in San Francisco</h2>
+          
+        </a>
+        <a href="">
+          <img src="" alt="" />
+          <h2>Event in Barcelona</h2>
+        </a>
+        </div>
+    </div>
 }
 export default Page;
